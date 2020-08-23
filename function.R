e3512778aee1b7c0f1c72c4d57535fdd47f5e8aa
Lan Anh <59382260+lananhcity93@users.noreply.github.com>
2020-08-23 10:49:24 +0700

    Add files via upload

---
+++ function.R
@@ -0,0 +1,9 @@
+sum_even.function  = function(start, end){
+    sum_even = 0
+    for(i in start:end){
+        if(i%%2 == 0){
+            sum_even = sum_even + i
+        }
+    }
+    return(sum_even)
+}
